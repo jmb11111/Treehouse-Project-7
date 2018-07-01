f66aa2a3c25efb72e6a3229c592fb30d24d9c62c
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-01 13:13:40 -0400

    all functions working, messages display, tweets send

--- js/app.js
+++ js/app.js
@@ -3,16 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         'fSUa8sb97bhxl1j1xZNtMAyDb',
-  consumer_secret:      '6kZxAtsGogv1KqAYJ2bdkyEKXZPsdlZsQ7SaWLVmb5DDOxsb1b',
-  access_token:         '18651378-lm8ONnfuYeX1c7n0cO0Jh2Gsih5CVFcMtPfQ6A8aL',
-  access_token_secret:  'qiADuQmB4PLqR2QbpIQecy9s0UNhkdvjqTxc48f2X9VdL',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-});
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,137 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+
+router.use(bodyParser.urlencoded({ extended: false}));
+router.use(bodyParser.json());
 
 var Twit = require('twit');
 var config = require('../config.js');
 
 var T = new Twit(config);
-
-
 let tweet = [];
 let follower = [];
 let followerDataArray;
-let messageText;
+
 let messageSenderPicture;
 var d = new Date();
 var n = d.getUTCHours();
-console.log(n);
+let currentUserID;
+
+let directMessagesArray;
+let userMessage = [];
+let recievedMessage = [];
+let currentUserPic;
+
+// T.get('users/lookup', { screen_name: "josh121592" }, function (err, currentUser, response) {
+//     if (!err) {
+//         // console.log(currentUser[0].id_str);
+//         // console.log(currentUser[0].profile_image_url);
+//         currentUserID = currentUser[0].id_str;
+//         currentUserPic = currentUser[0].profile_image_url;
+//         T.get('direct_messages/events/list', { id: currentUserID }, function (err, dmData, response) {
+//             if (!err) {
+//                 directMessagesArray = dmData;
+
+//                 // console.log(directMessagesArray);
+
+//                 for (let i = 0; i < dmData.events.length; i++) {
+
+//                     if (dmData.events[i].message_create.sender_id === currentUserID) {
+//                         userMessageID.push(dmData.events[i].id);
+//                         // console.log('user'+userMessageID);
+
+//                     } else {
+//                         recievedMessageID.push(dmData.events[i].id);
+//                         // console.log('recieved'+recievedMessageID);
+
+//                     }
+//                 }
 
 
-T.get('statuses/user_timeline', { screen_name: 'josh121592', count: 10 }, function (err, data, response) {
+//                 for (let i = 0; i < userMessageID.length; i++) {
+//                     T.get('direct_messages/events/show', { id: userMessageID[i] }, function (err, dMessage, response) {
+//                         if (!err) {
+//                             userMessage.push({
+//                                 id: dMessage.event.message_create.sender_id,
+//                                 text: dMessage.event.message_create.message_data.text,
+//                                 date: new Date(dMessage.event.created_timestamp*1000)
+
+//                             });
+//                             // console.log(userMessage);
+//                         } else {
+//                             console.log('fail 2' + err);
+//                         }
+//                     });
+//                 } for (let i = 0; i < recievedMessageID.length; i++) {
+//                     T.get('direct_messages/events/show', { id: recievedMessageID[i] }, function (err, reMessage, response) {
+
+//                         console.log(reMessage);
+
+//                         recievedMessage.push({
+//                             id: reMessage.event.message_create.sender_id,
+//                             text: reMessage.event.message_create.message_data.text,
+//                             date: new Date(reMessage.event.created_timestamp*1000)
+
+//                         });
+//                         // console.log(recievedMessage);
+//                         for (let i = 0; i < recievedMessage.length; i++) {
+//                             T.get('users/lookup', { user_id: recievedMessage[i].id }, function (err, senderUser, response) {
+//                                 recievedMessage[i].profile_image_url = senderUser[0].profile_image_url;
+//                                 console.log(recievedMessage);
+//                             })
+//                         }
+//                     });
+//                 }
+//             } else {
+//                 console.log('fail1' + err);
+//             }
+//         });
+//     } else {
+//         console.log('userInfo' + err);
+//     }
+// });
+
+
+T.get('statuses/home_timeline', { screen_name: 'josh121592', count: 10 }, function (err, data, response) {
     if (!err) {
         twitterFeedData = data;
+        for (let i = 0; i < twitterFeedData.length; i++) {
+            tweet[i] = {
+                text: twitterFeedData[i].text,
+                name: twitterFeedData[i].user.name,
+                screenName: twitterFeedData[i].user.screen_name,
+                userpic: twitterFeedData[i].user.profile_image_url,
+                retweetCount: twitterFeedData[i].retweet_count,
+                favoriteCount: twitterFeedData[i].favorite_count,
+                tweetLink: twitterFeedData[i].user.url,
+                retweetLink: twitterFeedData[i].id_str,
+                timeStamp: twitterFeedData[i].created_at.slice(0, 19),
+                retweet: '/retweet' + twitterFeedData[i].id_str,
+                like: '/like' + twitterFeedData[i].id_str,
+                retweetFunction: router.get('/retweet' + twitterFeedData[i].id_str, (req, res) => {
+                    T.post('statuses/retweet/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
+                        console.log('retweeted');
+                    })
+                    res.redirect('/');
+                }),
+                likeFunction: router.get('/like' + twitterFeedData[i].id_str, (req, res) => {
+                    T.post('favorites/create/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
+                        console.log('favorited');
+                    })
+                    res.redirect('/');
+                })
+            }
+        };
     } else {
         console.log(data);
-    } for (let i = 0; i < twitterFeedData.length; i++) {
-        tweet[i] = {
-            text: twitterFeedData[i].text,
-            name: twitterFeedData[i].user.name,
-            screenName: twitterFeedData[i].user.screen_name,
-            userpic: twitterFeedData[i].user.profile_image_url,
-            retweetCount: twitterFeedData[i].retweet_count,
-            favoriteCount: twitterFeedData[i].favorite_count,
-            tweetLink: twitterFeedData[i].user.url,
-            retweetLink: twitterFeedData[i].id_str,
-            timeStamp: n - Number(twitterFeedData[i].created_at.slice(11, 13)) + 'h',
-            retweet: '/retweet' + twitterFeedData[i].id_str,
-            like: '/like' + twitterFeedData[i].id_str,
-            retweetFunction: router.get('/retweet' + twitterFeedData[i].id_str, (req, res) => {
-                T.post('statuses/retweet/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
-                    console.log('retweeted');
-                })
-                res.redirect('/');
-            }),
-            likeFunction: router.get('/like' + twitterFeedData[i].id_str, (req, res) => {
-                T.post('favorites/create/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
-                    console.log('favorited');
-                })
-                res.redirect('/');
-            })
-        }
-    };
+    } 
 });
 
-T.get('friends/list', { screen_name: 'josh121592', count: 70 }, function (err, followData, response) {
+T.get('followers/list', { screen_name: 'josh121592', count: 70 }, function (err, followData, response) {
     if (!err) {
         followerDataArray = followData.users;
-        // console.log(followerDataArray[1]);
     } else {
         console.log(followData);
     } for (let i = 0; i < followerDataArray.length; i++) {
@@ -83,42 +160,37 @@ T.get('friends/list', { screen_name: 'josh121592', count: 70 }, function (err, f
     };
 });
 
-T.get('direct_messages/events/list', function (err, dmData, response) {
-    if (!err) {
-        let senderID = dmData.events[0].message_create.sender_id;
-        let messageID = dmData.events[0].id;
-        T.get('direct_messages/events/show', { id: messageID }, function (err, dMessage, response) {
-            if (!err) {
-                messageText = dMessage.event.message_create.message_data.text;
-                console.log(messageText);
-            } else {
-                console.log('dm events'+err);
-            }
-        }); 
-            T.get('users/lookup', { user_id: senderID }, function (err, messageSender, response) {
-                if (!err) {
-                    console.log(messageSender[0].profile_image_url);
-                    messageSenderPicture = messageSender[0].profile_image_url;
-                } else {
-                    console.log('userInfo'+err);
-                }
-            });
-    } else {
-        console.log(dmData);
-    }
-});
+
+
 
 
 router.get('/', (req, res) => {
-    dmMessageTextSent = messageText;
+    console.log(directMessagesArray);
+    profileImg = currentUserPic;
+    userID = currentUserID;
+    dmRecievedArray = recievedMessage;
+    dmSentArray = userMessage;
     following = followerDataArray.length;
     tweetText = tweet;
     followerInfo = follower;
-    messageSenderPictureURL= messageSenderPicture;
+    messageSenderPictureURL = messageSenderPicture;
     res.render('index');
 
 });
-
+router.post('/',function(req,res){
+  
+    console.log(req.body);
+    T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
+        if(!err){
+        console.log('tweeted')
+        res.render('index');
+        }else{
+            console.log("unable to post");
+            
+        }
+      })
+    
+  });
 
 
 
