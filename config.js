47667492b18614acbb4ac8c4e52d2b87e1638857
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-02 15:12:03 -0400

    removed secret stuff

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 
 
 module.exports=  {
-    consumer_key: 'fSUa8sb97bhxl1j1xZNtMAyDb',
-    consumer_secret: '6kZxAtsGogv1KqAYJ2bdkyEKXZPsdlZsQ7SaWLVmb5DDOxsb1b',
-    access_token: '18651378-lm8ONnfuYeX1c7n0cO0Jh2Gsih5CVFcMtPfQ6A8aL',
-    access_token_secret: 'qiADuQmB4PLqR2QbpIQecy9s0UNhkdvjqTxc48f2X9VdL',
-    userName: 'josh121592',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
+    userName: '',
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL: true,     // optional - requires SSL certificates to be valid.
 };;
