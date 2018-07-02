08b9487821cec9c5d050e515602d1f0127a08d04
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-02 15:09:29 -0400

    added notation

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-
+const Twit = require('twit');
+const config = require('../config.js');
+const T = new Twit(config);
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-var Twit = require('twit');
-var config = require('../config.js');
 
-var T = new Twit(config);
 let tweet = [];
 let follower = [];
 let followerDataArray;
-
 let messageSender= '';
-var d = new Date();
-var n = d.getUTCHours();
 let currentUserID = '';
 let currentUserUsername= config.userName;
 let directMessagesArray;
@@ -25,13 +21,15 @@ let currentUserPic= '';
 let dms =[];
 let messageSenderArray=[];
 
+//gets current user info based on user screen name supplied from config file
+
 T.get('users/lookup', { screen_name: currentUserUsername }, function (err, currentUser, response) {
     if (!err) {
-        // console.log(currentUser[0].id_str);
-        // console.log(currentUser[0].profile_image_url);
+  
         currentUserID = currentUser[0].id_str;
         currentUserPic = currentUser[0].profile_image_url;
         currentUserName =currentUser[0].name;
+        //gets direct messages from current user
         T.get('direct_messages/events/list', { id: currentUserID }, function (err, dmData, response) {
             if (!err) {
                 directMessagesArray = dmData.events;
@@ -41,7 +39,7 @@ T.get('users/lookup', { screen_name: currentUserUsername }, function (err, curre
                         dms.push(dm);
                        
                         for (let index = 0; index < dms.length; index++) {
-           
+                            //gets info on user who sent messages and adds picture, and date created
                             T.get('users/lookup', { user_id: dms[index].message_create.sender_id }, function (err, messageInfo, response) {
                             
                                
@@ -72,11 +70,12 @@ T.get('users/lookup', { screen_name: currentUserUsername }, function (err, curre
 });
 
 
-
+//gets 10 most recent tweets from the current user
 
 T.get('statuses/home_timeline', { screen_name: currentUserID, count: 10 }, function (err, data, response) {
     if (!err) {
         twitterFeedData = data;
+        // sets object properties of each tweet to populate pug template
         for (let i = 0; i < twitterFeedData.length; i++) {
             tweet[i] = {
                 text: twitterFeedData[i].text,
@@ -90,12 +89,14 @@ T.get('statuses/home_timeline', { screen_name: currentUserID, count: 10 }, funct
                 timeStamp: twitterFeedData[i].created_at.slice(0, 19),
                 retweet: '/retweet' + twitterFeedData[i].id_str,
                 like: '/like' + twitterFeedData[i].id_str,
+                //adds functionality to retweet button
                 retweetFunction: router.get('/retweet' + twitterFeedData[i].id_str, (req, res) => {
                     T.post('statuses/retweet/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
                         console.log('retweeted');
                     })
                     res.redirect('/');
                 }),
+                // adds functionality to like button
                 likeFunction: router.get('/like' + twitterFeedData[i].id_str, (req, res) => {
                     T.post('favorites/create/', { id: twitterFeedData[i].id_str }, function (err, data, respons) {
                         console.log('favorited');
@@ -108,7 +109,7 @@ T.get('statuses/home_timeline', { screen_name: currentUserID, count: 10 }, funct
         console.log(data);
     }
 });
-
+// gets current friend list of user
 T.get('friends/list', { screen_name: currentUserID, count: 70 }, function (err, followData, response) {
     if (!err) {
         followerDataArray = followData.users;
@@ -144,7 +145,7 @@ T.get('friends/list', { screen_name: currentUserID, count: 70 }, function (err, 
     }
 });
 
-
+// gets name of DM sender conversation
 messageSender = messageSenderArray.find(function(senderName){
                                     
     return senderName !== currentUserName;
@@ -152,6 +153,8 @@ messageSender = messageSenderArray.find(function(senderName){
 router.get('/error',(req, res)=>{
     res.render('error');
 })
+
+// loads page
 router.get('/', (req, res) => {
     if(messageSender !== '' && currentUserPic !== '' && currentUserID !== '' && dms !== [] && followerDataArray.length !== 0 && tweet !== [] && follower !== [] ){
     UserName =currentUserUsername;
@@ -168,6 +171,7 @@ router.get('/', (req, res) => {
         res.redirect('/error')
     }
 });
+//adds functionality to tweet box in bottom
 router.post('/', function (req, res) {
 
     console.log(req.body);
